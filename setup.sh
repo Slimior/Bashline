c5da9b97c27c163760cf183e608fb28ee8aa4c71
Slimior <slimior95@gmail.com>
2016-03-04 03:23:52 +0100

    improved setup script.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,15 @@ cp bashline ~/.bashline
 mkdir -p ~/.config/bashline
 cp blocks ~/.config/bashline/blocks
 
-echo Adding bashline to .bashrc.
+if [ -e ~/.bashrc ] ; then
+    if grep bashline ~/.bashrc > /dev/null ; then
+        echo Bashline already in .bashrc.
+    else
+        echo Adding bashline to .bashrc.
+        echo '. ~/.bashline' >> ~/.bashrc
+    fi
+else
+    echo 'No .bashrc found. Please add manually.'
+fi
 
-echo '. ~/.bashline' >> ~/.bashrc
-
-echo Done.
+echo Done. Refresh by running '". ~/.bashline"'
